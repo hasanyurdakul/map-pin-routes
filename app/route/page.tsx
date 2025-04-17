7623dc9b62518ea89e591bf74af85893d1b211ab
Hasan Yurdakul <65603504+hasanyurdakul@users.noreply.github.com>
2025-04-17 21:38:57 +0300

    update: rename -> RoutePage

--- app/route/page.tsx
+++ app/route/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const page = () => {
-  return <div>page</div>;
+const RoutePage = () => {
+  return <div>RoutePage</div>;
 };
 
-export default page;
+export default RoutePage;
