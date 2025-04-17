4658bed74c6b478cf1b278a9d408848cece7916e
Hasan Yurdakul <65603504+hasanyurdakul@users.noreply.github.com>
2025-04-17 21:35:19 +0300

    add: .prettierrc

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
-"use client";
-import styles from "./page.module.css";
-import { Button, HStack } from "@chakra-ui/react"
+'use client';
+import styles from './page.module.css';
+import { Button, HStack } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-        <HStack>
-      <Button onClick={()=>{console.log("Hello")}}>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+      <HStack>
+        <Button
+          onClick={() => {
+            console.log('Hello');
+          }}
+        >
+          Click me
+        </Button>
+        <Button>Click me</Button>
+      </HStack>
     </div>
   );
 }
